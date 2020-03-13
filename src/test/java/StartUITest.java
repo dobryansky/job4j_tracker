595bb2be8762dc876d983280880fedddba5a9c61
dobryansky <mikstrip@yandex.ru>
2020-03-13 17:55:27 +0300

    5.1. Тестирование. Подготовка данных.[#226225]

--- src/test/java/StartUITest.java
+++ src/test/java/StartUITest.java
@@ -15,5 +15,34 @@ public class StartUITest {
         assertThat(created.getName(), is(expected.getName()));
     }
 
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(), // id сохраненной заявки в объект tracker.
+                "replaced item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId() // id удаляемой заявки в объект tracker.
+
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(item.getId());
+        String expected = null;
+        assertThat(deleted, is(expected));
+    }
 
 }
